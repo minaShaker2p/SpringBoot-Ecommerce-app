fb115bbf592a321504b902ca3351aeef1ac20a1c
Mina Rezkalla <minashakeralfy@gmail.com>
2020-10-22 13:04:06 +0200

    add UsernameAndPasswordAuthenticationRequest and Filter

---
+++ src/main/java/com/mina/springbootecommerceapp/jwt/UsernameAndPasswordAuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.mina.springbootecommerceapp.jwt;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UsernameAndPasswordAuthenticationRequest {
+
+    private String username;
+    private String password;
+
+}
